8e7c4dbb2c4ecd60cd1927c4d76e57eb05e7187a
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-20 15:35:56 +0300

    initial commit

---
+++ src/main/java/com/subscriptor/SubscriptorApplication.java
@@ -0,0 +1,13 @@
+package com.subscriptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptorApplication.class, args);
+    }
+
+}
