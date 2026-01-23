405e938b40914ae622060fe733afd74fad83e38c
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-23 20:10:54 +0300

    feat: create mapper for user

---
+++ src/main/java/com/subscriptor/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.subscriptor.mapper;
+
+import com.subscriptor.dto.user.UserCreateRequest;
+import com.subscriptor.dto.user.UserResponse;
+import com.subscriptor.entity.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserCreateRequest userCreateRequest) {
+        User user = new User();
+        user.setEmail(userCreateRequest.getEmail());
+        user.setFirstName(userCreateRequest.getFirstName());
+        user.setLastName(userCreateRequest.getLastName());
+        user.setPasswordHash(userCreateRequest.getPassword()); //todo password encoder
+
+        return user;
+    }
+
+    public UserResponse toResponse(User user) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setRole(user.getRole());
+
+        return userResponse;
+    }
+}
