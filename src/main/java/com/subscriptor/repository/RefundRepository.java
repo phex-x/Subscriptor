70c44092fdb52e130062f29110baf78a411a8494
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-23 19:51:12 +0300

    fix: add decorator @Repository

--- src/main/java/com/subscriptor/repository/RefundRepository.java
+++ src/main/java/com/subscriptor/repository/RefundRepository.java
@@ -2,6 +2,8 @@ package com.subscriptor.repository;
 
 import com.subscriptor.entity.refund.Refund;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefundRepository extends JpaRepository<Refund, Long> {
 }
